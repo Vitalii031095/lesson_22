4c90fdaba188bee7eb1f8794d4121d1421dcfc4d
Vitalii <112901030+Vitalii031095@users.noreply.github.com>
2022-11-30 00:54:26 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,21 @@
+"use strict"
+// lesson 2;
+console.log("Учу \"JS\"")
+// lesson 2 - end;
+
+//урок 3
+
+// 1) Придумай имя переменной для хранения цвета
+// 	глаз пользователя сайта
+let colourEye;
+// 2) Объяви две переменных - user и userName.
+// 	Присвой значение "Вася" переменной userName.
+// 	Скопируй значение переменной userName в переменную user.
+// 	Выведи в консоль переменную user. Результат должен быть Вася
+
+let user,userName;
+userName = "Вася";
+user = userName;
+console.log(user)
+
+//урок 4
